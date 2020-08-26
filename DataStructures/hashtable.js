6a230e869f31bc14fc01c44bcc30ed273985c953
ddot727 <ddot727@gmail.com>
2020-08-25 21:09:33 -0400

    hash function built, skelly class made

---
+++ DataStructures/hashtable.js
@@ -0,0 +1,31 @@
+const hash = (key, size) => {
+  let hashedKey = 0
+
+  for (let i = 0; i < key.length; i++) {
+    hashedKey = key.charCodeAt(i)
+  }
+  return haskedKey % size
+}
+
+class HashTable {
+  constructor() {
+    this.size = 20
+    this.buckets = Array(this.size)
+
+    for (let i = 0; i < this.buckets.length; i++) {
+      this.buckets[i] = new Map()
+    }
+  }
+
+  insert() {
+
+  }
+
+  remove() {
+
+  }
+
+  search() {
+
+  }
+}
